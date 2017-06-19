89f32b5e1de76a374ad8d5595fb6af4890a47f0d
Keisuke KITA <kei.kita2501@gmail.com>
2017-06-19 19:34:12 +0900

    Source map support

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,13 @@
 external fun require(module:String):dynamic
 
+
 fun main(args: Array<String>) {
+    require("source-map-support").install()
     println("Hello JavaScript!")
 
     val express = require("express")
     val app = express()
+    app.hoge()
 
     app.get("/", { req, res ->
         res.type("text/plain")
